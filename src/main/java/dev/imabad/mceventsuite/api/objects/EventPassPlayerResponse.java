d3314fe820aa3230e43cef3c5b370cd71e961448
Stuart Pomeroy <stuart@pomeroys.site>
2021-09-17 19:19:25 +0100

    Add more data

--- src/main/java/dev/imabad/mceventsuite/api/objects/EventPassPlayerResponse.java
+++ src/main/java/dev/imabad/mceventsuite/api/objects/EventPassPlayerResponse.java
@@ -2,6 +2,7 @@ package dev.imabad.mceventsuite.api.objects;
 
 import dev.imabad.mceventsuite.core.EventCore;
 import dev.imabad.mceventsuite.core.api.objects.EventPlayer;
+import dev.imabad.mceventsuite.core.modules.eventpass.EventPassModule;
 import dev.imabad.mceventsuite.core.modules.eventpass.db.EventPassReward;
 import dev.imabad.mceventsuite.core.modules.mysql.MySQLModule;
 import dev.imabad.mceventsuite.core.modules.scavenger.db.ScavengerDAO;
@@ -15,6 +16,11 @@ public class EventPassPlayerResponse {
 
     private int xp;
     private int level;
+    private int levelXp;
+    private int xpToNextLevel;
+    private int nextLevelXp;
+    private int xpSinceLevel;
+    private double progressToNextLevel;
     private EventPlayer player;
     private List<EventPlayerYearSlim> years;
 
@@ -23,6 +29,12 @@ public class EventPassPlayerResponse {
     public EventPassPlayerResponse(int xp, int level, EventPlayer player, Set<ScavengerLocation> foundLocations) {
         this.xp = xp;
         this.level = level;
+        this.levelXp = EventPassModule.experience(level);
+        this.nextLevelXp = EventPassModule.experience(level + 1);
+        this.xpToNextLevel = (nextLevelXp - xp);
+        int nextLevelUp = nextLevelXp - levelXp;
+        this.xpSinceLevel = xp - levelXp;
+        this.progressToNextLevel = (((double)xpSinceLevel / nextLevelUp)) * 100;
         this.player = player;
         this.years = player.getAttendance().stream().map(eventPlayerYear -> new EventPlayerYearSlim(eventPlayerYear.getYear(), eventPlayerYear.getRank())).collect(Collectors.toList());
         this.foundLocations = foundLocations;
