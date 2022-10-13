99a9ea0072dc21794d884284ce009ad63c428f93
Ben Houghton <bennixerus@gmail.com>
2022-10-13 18:11:59 +0100

    Add unlocks and set level

--- src/main/java/dev/imabad/mceventsuite/api/controllers/BoothController.java
+++ src/main/java/dev/imabad/mceventsuite/api/controllers/BoothController.java
@@ -14,6 +14,11 @@ import dev.imabad.mceventsuite.core.EventCore;
 import dev.imabad.mceventsuite.core.api.objects.EventBooth;
 import dev.imabad.mceventsuite.core.api.objects.EventPlayer;
 import dev.imabad.mceventsuite.core.api.objects.EventRank;
+import dev.imabad.mceventsuite.core.modules.eventpass.EventPassModule;
+import dev.imabad.mceventsuite.core.modules.eventpass.db.EventPassDAO;
+import dev.imabad.mceventsuite.core.modules.eventpass.db.EventPassPlayer;
+import dev.imabad.mceventsuite.core.modules.eventpass.db.EventPassReward;
+import dev.imabad.mceventsuite.core.modules.eventpass.db.EventPassUnlockedReward;
 import dev.imabad.mceventsuite.core.modules.mysql.MySQLModule;
 import dev.imabad.mceventsuite.core.modules.mysql.dao.BoothDAO;
 import dev.imabad.mceventsuite.core.modules.mysql.dao.PlayerDAO;
@@ -112,6 +117,29 @@ public class BoothController {
                         EventCore.getInstance().getModuleRegistry().getModule(MySQLModule.class).getMySQLDatabase().getDAO(PlayerDAO.class).saveOrUpdatePlayer(eventPlayer);
                         EventCore.getInstance().getModuleRegistry().getModule(RedisModule.class).publishMessage(RedisChannel.GLOBAL, new UpdatedPlayerMessage(eventPlayer.getUUID()));
                     }
+
+
+                    EventPassModule eventPassModule = EventCore.getInstance().getModuleRegistry().getModule(EventPassModule.class);
+                    EventPassDAO eventPassDAO = EventCore.getInstance().getModuleRegistry().getModule(MySQLModule.class).getMySQLDatabase()
+                            .getDAO(EventPassDAO.class);
+
+                    EventPassPlayer eventPassPlayer = eventPassDAO.getOrCreateEventPass(eventPlayer);
+                    if(eventPassPlayer.levelFromXP() < 50) {
+                        eventPassPlayer.setXp(EventPassModule.experience(50));
+                        eventPassDAO.saveEventPassPlayer(eventPassPlayer);
+                    }
+
+                    List<EventPassReward> unlocks = eventPassDAO.getUnlockedRewards(eventPlayer).stream().map(EventPassUnlockedReward::getUnlockedReward).toList();
+                    Set<EventPassUnlockedReward> toUnlock = new HashSet<>();
+                    for(EventPassReward reward : eventPassModule.getEventPassRewards()) {
+                        if(!unlocks.contains(reward)) {
+                            EventPassUnlockedReward unlockedReward = new EventPassUnlockedReward(reward, eventPlayer);
+                            toUnlock.add(unlockedReward);
+                        }
+                    }
+                    if(toUnlock.size() > 0)
+                        eventPassDAO.saveUnlockedRewardBatch(toUnlock);
+
                     EventCore.getInstance().getModuleRegistry().getModule(RedisModule.class).publishMessage(RedisChannel.GLOBAL, new DonationMessage(customer.get("ign").getAsString(), DonationMessage.Type.VIPP, 0, true));
                     processedPackage = true;
                 } else if(packageID == 2099957) {
